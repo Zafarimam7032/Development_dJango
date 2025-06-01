1147d45dd11e56d82c4acf111e25fd198be4e050
Zafar imam <zafarimam7032@gmail.com>
2025-06-01 18:29:47 +0530

    hmtl

--- FirstProject/FirstProject/view.py
+++ FirstProject/FirstProject/view.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 def viewPage(request):
   print("this is first view first project")
-  return HttpResponse("<h1>this is view page</h1>")
+  # return HttpResponse("<h1>this is view page</h1>")
+  return render(request,"Home.html",{})
